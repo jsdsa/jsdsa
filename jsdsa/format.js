5d40856790ba10b6585e26741240110d3ca39228
ironmaniiith <aalekhj2507@gmail.com>
2016-07-16 06:32:13 +0530

    added `format.js`
    follow this format for all the other codes

---
+++ jsdsa/format.js
@@ -0,0 +1,33 @@
+/**
+ * <Name>:
+ *  Class: <Class of Data Structure or Algorithm>
+ *  Data Structure: <Data Structure involved>
+ *  Time Complexity: <Average Time Complexity Involved>
+ *  Description: <Description about the Algorithm or Data Structure>
+ *  Resources: <Resources for more detailed understanding, preferrable wikipedia link>
+ *  Extras: <Other relevant information>
+ */
+
+/** Cache some methods to call later */
+
+/**
+ *  <Function Name>: 
+ *      <Short Description about the Function>
+ *
+ *  Required:
+ *  @param {Type} [<param Name>] <Short Param Description>
+ *
+ *  Optional:
+ *  @param {Type} [<param Name>] <Short Param Description>
+ *
+ *  Undocumented:
+ *  @param {Type} [<param Name>] <Short Param Description>
+ *
+ *  @returns {Type} <Short Description of what the Function returns>
+ */
+function Name(arguments) {
+    // code
+}
+
+/** Export the `<Name>` function */
+module.exports.Name = Name;
