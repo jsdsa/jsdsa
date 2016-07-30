3361cf922f4dd30d78d28f76315e7c9d6a60f96b
Aalekh Jain <aalekhj2507@gmail.com>
2016-07-30 22:14:41 +0530

    Revert "Added insertionSort.js"

--- jsdsa/index.js
+++ jsdsa/index.js
@@ -2,4 +2,3 @@ module.exports.binarySearch = require('./binarySearch').binarySearch;
 module.exports.linearSearch = require('./linearSearch').linearSearch;
 module.exports.mergeSort = require('./mergeSort').mergeSort;
 module.exports.selectionSort = require('./selectionSort').selectionSort;
-module.exports.insertionSort = require('./insertionSort').insertionSort;
