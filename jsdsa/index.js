c042d5965bc2cdc83f4e2d818ffdd5ef6ff56a0d
Aalekh Jain <aalekhj2507@gmail.com>
2016-07-30 23:43:41 +0530

    Added bubbleSort.js (#13)
    * added `bubbleSort.js`
    
    * added tests for `bubbleSort.js`
    
    * index.js update

--- jsdsa/index.js
+++ jsdsa/index.js
@@ -3,3 +3,4 @@ module.exports.linearSearch = require('./linearSearch').linearSearch;
 module.exports.mergeSort = require('./mergeSort').mergeSort;
 module.exports.selectionSort = require('./selectionSort').selectionSort;
 module.exports.insertionSort = require('./insertionSort').insertionSort;
+module.exports.bubbleSort = require('./bubbleSort').bubbleSort;
