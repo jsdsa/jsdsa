5a3202184796be7d59a1b84a6548efd89c75911b
ironmaniiith <aalekhj2507@gmail.com>
2016-07-30 20:20:49 +0530

    index.js update

--- jsdsa/index.js
+++ jsdsa/index.js
@@ -1,2 +1,4 @@
 module.exports.binarySearch = require('./binarySearch').binarySearch;
 module.exports.linearSearch = require('./linearSearch').linearSearch;
+module.exports.mergeSort = require('./mergeSort').mergeSort;
+module.exports.selectionSort = require('./selectionSort').selectionSort;
