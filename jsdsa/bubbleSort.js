289b7e8c614ef0985e4b865b86cb5ec3d6363dc4
ironmaniiith <aalekhj2507@gmail.com>
2016-07-30 23:36:32 +0530

    added `bubbleSort.js`

---
+++ jsdsa/bubbleSort.js
@@ -0,0 +1,45 @@
+/**
+ * Bubble Sort: 
+ *  Class: Sorting
+ *  Data Structure: Array
+ *  Time Complexity: O(n^2)
+ *  Description: Bubble sort is a simple sorting algorithm that sorts the array by comparing adjacent items
+ *  Resources: https://en.wikipedia.org/wiki/Bubble_sort
+ *  Extras: 
+ *  Contributors: <@ironmaniiith aalekhj2507@gmail.com>
+ */
+
+/** Require calls */
+
+/** Cache methods or assign Globals */
+
+/**
+ *  bubbleSort:
+ *      Sorts the given array using bubble sort algorithm
+ * 
+ *  Required:
+ *  @param {Array} [array] array to sort
+ * 
+ *  Optional:
+ *  @param {Number} [low] lower index of the array to sort
+ *  @param {Number} [up] upper index of the array to sort
+ * 
+ *  @returns {undefined} Sorts the given array, returns nothing
+ */
+function bubbleSort(array, low, up) {
+	low = low || 0;
+	up = up || array.length - 1;
+	
+	for (var i = low; i < up; i++) {
+		for (var j = low; j < up - i; j++) {
+			if (array[j] > array[j + 1]) {
+				var temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
+
+/** Export the function as module */
+module.exports.bubbleSort = bubbleSort;
