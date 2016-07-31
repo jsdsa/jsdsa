7aef7252241683fa1e27be9b324e80911d9870e2
ironmaniiith <aalekhj2507@gmail.com>
2016-07-31 15:51:43 +0530

    added `countingSort.js`

---
+++ jsdsa/countingSort.js
@@ -0,0 +1,49 @@
+/**
+ * Counting Sort: 
+ *  Class: Sorting
+ *  Data Structure: Array
+ *  Time Complexity: O(n+k), where k is the maximum value of key in the array
+ *  Description: Counting sort is an algorithm for sorting a collection of objects based on key values (small)
+ *  Resources: https://en.wikipedia.org/wiki/Counting_sort
+ *  Extras: 
+ *  Contributors: <@ironmaniiith aalekhj2507@gmail.com>
+ */
+
+/** Require calls */
+
+/** Cache methods or assign Globals */
+var MAX = 1e6 << 0;
+
+/**
+ *  countingSort:
+ *      Sorts the given array using counting sort algorithm
+ * 
+ *  Required:
+ *  @param {Array} [array] array to sort
+ * 
+ *  Optional:
+ *  @param {Number} [maxValue] maximum value of the key in the array
+ * 
+ *  @returns {undefined} Sorts the given array, returns nothing
+ */
+function countingSort(array, maxValue) {
+	maxValue = maxValue || MAX;
+	var countArray = new Array(maxValue + 1);
+
+	for (var i = 0; i <= maxValue; i++) countArray[i] = 0;
+
+	for (var i = 0; i < array.length; i++) {
+		countArray[array[i]]++; 
+	}
+	var count = 0,
+		index = 0;
+	while (count <= maxValue) {
+		while (countArray[count]--) {
+			array[index++] = count;
+		}
+		count++;
+	}
+}
+
+/** Export the function as module */
+module.exports.countingSort = countingSort;
