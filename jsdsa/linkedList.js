a36fdb3c449beec569e9229644e5373d6c648a90
ironmaniiith <aalekhj2507@gmail.com>
2016-08-05 02:25:17 +0530

    added Node constructor

---
+++ jsdsa/linkedList.js
@@ -0,0 +1,26 @@
+/**
+ * Linked list: 
+ *  Class: Linear Storage
+ *  Description: Linear collection of data elements
+ *  Resources: https://en.wikipedia.org/wiki/Linked_list
+ *  Extras: http://code.tutsplus.com/articles/data-structures-with-javascript-singly-linked-list-and-doubly-linked-list--cms-23392
+ *  Contributors: <@ironmaniiith aalekhj2507@gmail.com>
+ */
+
+/** Require calls */
+
+/** Cache methods or assign Globals */
+// TODO: push, pop, update, show, remove, reverse, circularLinkedList
+
+/**
+ *  Node:
+ *      Node constructor for linked list
+ */
+function Node() {
+	if (this instanceof Node) {
+		this.next = null;
+		this.val = 0;
+	} else {
+		return new Node();
+	}
+}
