7b8b2e965816fd29e6461e282cf2b1a09521d6bf
ironmaniiith <aalekhj2507@gmail.com>
2016-08-06 15:52:48 +0530

    added Queue constructor

---
+++ jsdsa/queue.js
@@ -0,0 +1,31 @@
+/**
+ * Queue: 
+ *  Class: FIFO-Data Structure
+ *  Time Complexity: NA
+ *  Description: A linear data structure that is based on first in first out principle, queue implemented using fixed size array
+ *  Resources: https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+ *  Extras: 
+ *  Contributors: <@ironmaniiith aalekhj2507@gmail.com>
+ */
+
+/** Require calls */
+
+/** Cache methods or assign Globals */
+
+/**
+ *  Queue:
+ *      Queue constructor
+ *
+ *  @param {Number} [capacity] capacity of the queue
+ */
+function Queue(capacity) {
+    if (this instanceof Queue) {
+        this.array = new Array(capacity);
+        this.size = 0;
+        this.capacity = capacity;
+        this.front = -1;
+        this.back = -1;
+    } else {
+        return new Queue(capacity);
+    }
+}
