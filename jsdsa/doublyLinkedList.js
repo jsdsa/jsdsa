2b0091738e850823a32932e507d967d7621a296c
ironmaniiith <aalekhj2507@gmail.com>
2016-08-08 03:45:15 +0530

    added insertStart

--- jsdsa/doublyLinkedList.js
+++ jsdsa/doublyLinkedList.js
@@ -43,3 +43,24 @@ function createNode(prev, next, val) {
     node.val = val;
     return node;
 }
+
+/**
+ *  insertStart:
+ *      Inserts a new node in the beginning of the linked list: O(1)
+ * 
+ *  Required:
+ *  @param {Node} [head] head node of the doubly linked list
+ *  @param {Number} [val] value to assign to the node
+ * 
+ *  @returns {Node} Returns the newly created head node of the linked list
+ */
+function insertStart(head, val) {
+    var node = createNode(null, head, val);
+    
+    if (!head) {
+        return node;
+    } else {
+        head.prev = node;
+        return node;
+    }
+}
